7634970534068530e9c2572c7b2abe3735cd9d1d
AshleyWWW <ashley.x.y.w@gmail.com>
2018-03-18 01:51:46 -0400

    world cloud images generated

--- wordGenerator.py
+++ wordGenerator.py
@@ -19,14 +19,15 @@ d = path.dirname(__file__)
 
 # Read the whole text.
 # for i in range(1, 106):
-for i in range(1, 10):
+for i in range(1, 55):
     print(i)
     file = 'tad_GO/tad' + str(i) + '.txt'
     text = ""
     try:
-        input_reader = csv.reader(open(path.join(d, file)), delimiter='\t')
-        for row in input_reader:
+        input_reader = open(path.join(d, file))
+        for row in csv.reader(input_reader, delimiter='\t'):
             text += row[3] + " "
+        input_reader.close()
 
         # read the mask image
         # taken from
@@ -39,6 +40,7 @@ for i in range(1, 10):
         stopwords.add("activity")
         stopwords.add("binding")
         stopwords.add("cell")
+        stopwords.add("process")
 
         wc = WordCloud(background_color="white", max_words=2000, mask=circle_mask,
                stopwords=stopwords, prefer_horizontal=1)
@@ -54,6 +56,7 @@ for i in range(1, 10):
         plt.figure()
         plt.imshow(circle_mask, cmap=plt.get_cmap('gray'), interpolation='bilinear')
         plt.axis("off")
+        plt.close()
         # plt.show()
-    except IOError as e:
-        print(str(i) + "is an empty TAD\n")
+    except:
+        print(str(i) + " is an empty TAD")
